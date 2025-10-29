472b58739fcb9f470902ccea852945d6faaf17db
bhammon75-collab <bhammon75@gmail.com>
2025-10-28 20:56:52 -0700

    Add files via upload

---
+++ core/js/contents.js
@@ -0,0 +1,38 @@
+/*
+ * contents.js
+ * Copyright (C) 2023 Element Davv<elementdavv@hotmail.com>
+ *
+ * Distributed under terms of the GPL3 license.
+ */
+
+'use strict';
+
+(() => {
+    if (window.internetarchivedownloaderinit === true) {
+        return;
+    }
+    const isarchive = location.origin.indexOf('archive.org') > -1;
+
+    if (isarchive) {
+        const mediatype = document.querySelector("meta[property='mediatype']");
+
+        // filter out collection pages
+        if (!mediatype || mediatype.content != "texts") {
+            chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {});
+            return;
+        }
+    }
+
+    if (typeof window.internetarchivedownloader === 'undefined' ) {
+        const url = isarchive ? 'js/archive.js' : 'js/hathitrust.js';
+        const src = chrome.runtime.getURL(url);
+
+        import(src).then( Contents => {
+            window.internetarchivedownloader = new Contents.default();
+            window.internetarchivedownloader.setup();
+        });
+    }
+    else
+        window.internetarchivedownloader.setup();
+
+})();
